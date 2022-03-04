b964ffae2e09802472deb04a85eb26163dd4e181
Longleafern <Nicholasenglehart@gmail.com>
2022-03-03 20:14:53 -0800

    Able to enter numbers non-divisible by 5 and get remainder NO LONGER CRASHES

--- java.js
+++ java.js
@@ -11,21 +11,24 @@ function calculate(weight) {
   while(barweight > 0)  { 
     if (barweight >= 90) {
             console.log("Two 45's")
-            barweight = (barweight - 90)
+            barweight =(barweight - 90)
         } if (barweight >= 70 && barweight < 90) {
             console.log("Two 35's")
-            barweight = (barweight - 70)
+            barweight =(barweight - 70)
         }  if (barweight >= 50 && barweight < 70) {
             console.log("Two 25's")
-            barweight = (barweight - 50)
+            barweight =(barweight - 50)
         }  if (barweight >= 20 && barweight < 50) {
             console.log("Two 10's")
             barweight = (barweight - 20)
         } if (barweight >= 10 && barweight < 20) {
             console.log("Two 5's")
-            barweight =  (barweight - 10)
+            barweight =(barweight - 10)
         } if (barweight >= 5 && barweight < 10) {
             console.log("Two 2.5's")
             barweight = (barweight - 5) 
-        }
-};  };
+        } if (barweight <= 4) {
+            console.log(`Weight Remaining ${barweight}`)
+             break
+        } 
+ };  };
